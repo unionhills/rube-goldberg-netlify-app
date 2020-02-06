1b0a014a0bac306e3287c643883566f42893b7e6
Union Hills Software <unionhills@cox.net>
2020-02-05 22:04:13 -0700

    Added model class for our message

---
+++ functions/api/shared/models/message.model.ts
@@ -0,0 +1,21 @@
+/**
+ * This class represents the concept of a Message in
+ * our application.  A Message simply contains a
+ * packet of data to be transported.
+ *
+ * @author Union Hills Software
+ * @date   February 5, 2020
+ *
+ */
+
+export class Message {
+  id: string;
+
+  subject: string;
+  body: string;
+
+  correlationId: string;
+  trace: [];
+  createdAt: Date;
+  updatedAt: Date;
+}
