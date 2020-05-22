48d816136ab6bd0a6217beac305105e80504d5a5
Union Hills Software <unionhills@cox.net>
2020-05-21 23:41:42 -0700

    Refactored unit test in functions api. Added more tests to cover functionality of the NoteRepository

---
+++ functions/api/test/repos/note.repo.test.ts
@@ -0,0 +1,51 @@
+import { Note } from '../../shared/models';
+import { NoteRepository } from '../../repos';
+
+describe("Let's try testing the repo", () => {
+  test("Check to see if we get records back from the NoteRepository", () => {
+    const noteRepo: NoteRepository = new NoteRepository();
+    const notes: Note[] = noteRepo.findAll();
+
+    expect(notes).toBeDefined();
+    expect(notes.length).toBeGreaterThan(0);
+  });
+});
+
+describe("Test create operation", () => {
+  test("Check to see if we can add a new note to the NoteRepository", () => {
+    const noteRepo: NoteRepository = new NoteRepository();
+    const notes: Note[] = noteRepo.findAll();
+    const recordCount = notes.length;
+    const newNote: Note = new Note();
+
+    newNote.body = "This is a test note";
+    noteRepo.create(newNote);
+
+    expect(noteRepo.findAll().length).toEqual(recordCount + 1);
+  });
+});
+
+describe("Test repo update operation", () => {
+  test("Check to if we can update a note properly in the NoteRepository", () => {
+    const noteRepo: NoteRepository = new NoteRepository();
+    const newBody: string = "This is the new body content";
+    let note: Note = noteRepo.findById("1");
+
+    note.body = newBody;
+    noteRepo.update(note.id, note);
+    note = noteRepo.findById("1");
+
+    expect(note.body).toEqual(newBody);
+  });
+});
+
+describe("Test repo update operation", () => {
+  test("Check to if we can delete a note properly in the NoteRepository", () => {
+    const noteRepo: NoteRepository = new NoteRepository();
+
+    noteRepo.delete("1");
+    const note: Note = noteRepo.findById("1");
+
+    expect(note).toBeUndefined();
+  });
+});
---
+++ functions/api/test/shared/models/note.model.test.ts
@@ -0,0 +1,11 @@
+import { Note } from '../../../shared/models';
+
+describe("This is a simple test of the note model", () => {
+  test("Check to see if imports are working", () => {
+    const note: Note = new Note();
+
+    note.id = "1"
+    expect(note.id).toEqual("1");
+  });
+});
+
