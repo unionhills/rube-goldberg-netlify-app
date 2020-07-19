9c824c15abdfc335e8ddf109659b39aa6f8ee1b6
Union Hills Software <unionhills@cox.net>
2020-07-18 22:40:28 -0700

    Changed test code to use async/await

--- functions/api/test/repos/note.repo.mongo.test.ts
+++ functions/api/test/repos/note.repo.mongo.test.ts
@@ -4,16 +4,27 @@ import dotenv from 'dotenv';
 
 beforeAll(async ()=> {
   dotenv.config();
-
+/*
   console.log(`DB_HOST=${process.env.DB_HOST}`);
   console.log(`DB_NAME=${process.env.DB_NAME}`);
   console.log(`DB_USER=${process.env.DB_USER}`);
   console.log(`DB_PASS=${process.env.DB_PASS}`);
-
-  const uri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+*/
+  const uri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true });
+    const connectOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      autoIndex: false, // Don't build indexes
+      poolSize: 10, // Maintain up to 10 socket connections
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    };
+
+    await mongoose.connect(uri, connectOptions);
     console.log('Successfully connected to MongoDB Atlas');
   } catch (error) {
     console.error(error);
@@ -28,23 +39,21 @@ afterAll(async ()=> {
 });
 
 describe("Let's see if we can retrieve the contents of the repo", () => {
-  test("Check to see if we get records back from the NoteRepository", done => {
+  test("Check to see if we get records back from the NoteRepository", async () => {
     const noteMongoRepo: NoteMongoRepository = new NoteMongoRepository(mongoose);
 
     const noteModel: Model<INote> = noteMongoRepo.model();
 
-    noteModel.find({}, (err, docs) => {
-      if (err) {
-        done(err);
-      }
-      else {
-        expect(docs).toBeDefined();
-        expect(docs.length).toBeGreaterThan(0);
+    try {
+      const docs = await noteModel.find({}).exec();
 
-        console.log(`docs=\n${docs}`);
-        done();
-      }
-    });
+      expect(docs).toBeDefined();
+      expect(docs.length).toBeGreaterThan(0);
+
+      console.log(`docs=\n${docs}`);
+    } catch (err) {
+      err.stack;
+    }
   });
 });
 
