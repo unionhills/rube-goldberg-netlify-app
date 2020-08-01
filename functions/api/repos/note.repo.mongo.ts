f6ca63279d888af605c82e635126676e1d09b8c7
Union Hills Software <unionhills@cox.net>
2020-08-01 07:03:36 -0700

    Implemented IRepository interface Added timestamp option to model

--- functions/api/repos/note.repo.mongo.ts
+++ functions/api/repos/note.repo.mongo.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
+import { IRepository } from '.';
 
 export interface INote extends Document {
     subject: string;
@@ -6,7 +7,7 @@ export interface INote extends Document {
     correlationId: string;
 }
 
-export class NoteMongoRepository {
+export class NoteMongoRepository implements IRepository<INote> {
     private static instance: NoteMongoRepository;
     readonly model: Model<INote>;
 
@@ -27,7 +28,7 @@ export class NoteMongoRepository {
             subject: { type: String },
             body: { type: String },
             correlationId: { type: String }
-        });
+        }, { timestamps: {} });
 
         return mongoose.model<INote>('Note', NoteSchema);
     }
@@ -36,8 +37,52 @@ export class NoteMongoRepository {
         try {
             const docs = await this.model.find({}).exec();
 
-            console.log(`docs=\n${docs}`);
+            console.log(`found docs=\n${docs}`);
             return docs;
+        } catch (err) {
+            err.stack;
+        }
+    }
+
+    public async findById(id: string): Promise<INote> {
+        try {
+            const doc = await this.model.findById(id).exec();
+
+            console.log(`found doc=\n${doc}`);
+            return doc;
+        } catch (err) {
+            err.stack;
+        }
+    }
+
+    public async create(newNote: INote): Promise<INote> {
+        try {
+            const doc = await this.model.create(newNote);
+
+            console.log(`created doc=\n${doc}`);
+            return doc;
+        } catch (err) {
+            err.stack;
+        }
+    }
+
+    public async update(id: string, note: INote): Promise<INote> {
+        try {
+            const doc = await this.model.findByIdAndUpdate(id, note);
+
+            console.log(`updated doc=\n${doc}`);
+            return doc;
+        } catch (err) {
+            err.stack;
+        }
+    }
+
+    public async delete(id: string): Promise<INote> {
+        try {
+            const doc = await this.model.findByIdAndDelete(id).exec();
+
+            console.log(`deleted doc =\n${doc}`);
+            return doc;
         } catch (err) {
             err.stack;
         }
