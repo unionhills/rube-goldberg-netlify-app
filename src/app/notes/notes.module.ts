67fa2ea49868b045e00fdca3afe525fcf177484b
Union Hills Software <unionhills@cox.net>
2020-09-05 15:12:40 -0700

    Added a notes service. Details will be filled in later.

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
 import { NotesComponent } from './notes.component';
-
+import { NotesService } from './notes.service';
 
 @NgModule({
   declarations: [NotesComponent],
   imports: [
     CommonModule,
     NotesRoutingModule
-  ]
+  ], providers: [NotesService]
 })
 export class NotesModule { }
