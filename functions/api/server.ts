98842b30f9b4e54505a258c3d4dff7a1370e7ffc
Union Hills Software <unionhills@cox.net>
2020-09-12 16:40:46 -0700

    Added ability to configure the persistence type. This way we can control whether or not we're using the internal in-memory db or connecting to MongoDB.

--- functions/api/server.ts
+++ functions/api/server.ts
@@ -4,18 +4,47 @@ import { Application } from 'express';
 import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
 
+import { NoteService } from './services';
+
+import { INote } from './shared';
+import {
+    IRepository,
+    MongoNoteRepository,
+    InMemoryNoteRepository
+} from './repos';
+
 import { ApiHealthController, NoteController } from './controllers';
 
 export class ApiServer extends Server {
     constructor() {
         super(true);
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: true }));
+
+        this.setupExpress();
         this.setupControllers();
     }
 
+    private getRepository(): NoteService {
+        let noteService: NoteService;
+
+        if (process.env.PERSISTENCE_TYPE == 'db') {
+            noteService = new NoteService(MongoNoteRepository.getInstance());
+        } else {
+            noteService = new NoteService(InMemoryNoteRepository.getInstance());
+        }
+
+        return noteService;
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+    }
+
     private setupControllers(): void {
-        const ctlrInstances = [new ApiHealthController(), new NoteController()];
+        const ctlrInstances = [
+            new ApiHealthController(),
+            new NoteController(this.getRepository())
+        ];
 
         super.addControllers(ctlrInstances);
     }
